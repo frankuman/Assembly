2b8f03019dcee775e3a8eadaecc964f51c753593
frankuman <oliverbolin97@gmail.com>
2023-04-12 15:28:44 +0200

    lab2-3 finished

--- Lab 1/lab2-3.s
+++ Lab 1/lab2-3.s
@@ -5,41 +5,41 @@
 @ else{
 @     return(1);
 @ }
-numbers:
-    .word	1,2,3,4,5,6,7,8,9,10,0
+numbers: //Our list, end with 0
+    .word   1,2,3,4,5,6,7,8,9,10,0
 
 .global _start
-.text
-factorial:
-    PUSH{ lr }
-    CMP r5,#1
-    MOVLE r5,#1
-    BLE factFinish:
-    SUB SP,SP,#4
-    MOV r6,r5
-    SUB r6,r6,#1
-    BL factorial
-    MUL r5,r6,r5
-    ADD SP,SP,#4
-    B factFinish
 
+factorial: // -> IN: r0 UT: r0
+    PUSH {r1,r2,lr}
+    CMP r0,#1
+    BEQ return
+    MOV r1,r0 //r1 = r0
+    SUB r0,r0,#1 // r1 - 1
+    BL factorial //recursion
+    MUL r2,r1,r0 // number = factorial(number - 1)*number
+    MOV r0,r2 //mov answer to r0
+    POP {r1,r2,pc} 
 
+return:
+    MOV r0,#1 //we go here when r0 = 1
+    POP {r1,r2,pc}
 
-factFinish:
-    POP{ pc }
 _start:
-    LDR r4, =numbers //load into r4
+    LDR r4, =numbers
     loop:
+        LDR r0, [r4],#4
+        CMP r0, #0
+        BEQ finish
         
-        LDR r5, [r4], #4 //we take out first int and move pointer
-        CMP r5, #0 // if 0 we finito
-        BEQ finish //move to finished if 0
         BL factorial
+        MOV r6,r0 //Show answer at r6 for simplicity
+
+    
         b loop
 
     finish:
         B _end
 _end:
     B _end
-
 .end
